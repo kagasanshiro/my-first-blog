325a264a6a0a2d5bf83d286b2dba07a074fb759b
kagasanshiro <kaga.san@galilei.co.jp>
2021-11-25 09:29:59 +0900

    My Django Girls app, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kaga.san\Desktop\勉強用\プログラミング\python\djangogirls\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
